cac70c23adb37581bf9cfd53d1fc49c762a50438
Ian Solomein <solomeinyan@gmail.com>
2022-09-18 18:50:44 +0300

    38

--- backend/api/management/commands/load_ingredients.py
+++ backend/api/management/commands/load_ingredients.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from api.models import Ingredient
+from recipes.models import Ingredient
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from django.db.utils import IntegrityError
